f496752832da2e2e3fe92c1e28727f35d6486b28
Joao Fiorini <joao.fiorini@gmail.com>
2025-03-17 23:17:58 -0300

    feat - tech icons

---
+++ src/components/TechHighlights/index.js
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from 'react'
+import style from './TechHighlights.module.css'
+import Icon from 'components/Icon'
+
+export default function TechHighlights() {
+
+    const [techName, setTechname] = useState('...')
+
+
+
+    return (
+        <div className={style.tecnologias}>
+            <div className={style.descricao}>
+
+                <h2>Desenvolvendo soluções</h2>
+                <h3>com carinho e{`${techName}`}</h3>
+            </div>
+            <ul className='icon-list'>
+                <li onMouseEnter={() => setTechname(' React.')}>
+                    <Icon id='react' size={100} color="#363636" key="React"></Icon>
+                </li>
+                <li onMouseEnter={() => setTechname(' Figma.')}>
+                    <Icon id='figma' size={100} color="#363636" key="Figma"></Icon>
+                </li>
+                <li onMouseEnter={() => setTechname(' Oracle.')}> 
+                    <Icon id='db' size={100} color="#363636" key="Oracle"></Icon>
+                </li>
+                <li onMouseEnter={() => setTechname(' Javascript.')}>
+                    <Icon id='js' size={100} color="#363636" key="Javascript"></Icon>
+                </li>
+                <li onMouseEnter={() => setTechname(' PHP.')}>
+                    <Icon id='php' size={100} color="#363636" key="PHP"></Icon>
+                </li>
+
+            </ul>
+        </div>
+    )
+}
